e0e0858e4c6305de791dccd893bb1d40b2f1c8e2
Guillaume Lambert <guillaume.p.lambert@gmail.com>
2016-08-02 17:59:21 +0200

    test heroku push

--- server.js
+++ server.js
@@ -14,7 +14,6 @@ app.use(bodyParser.json());
 // Create a database variable outside of the database connection callback to reuse the connection pool in your app.
 var db;
 
-
 // Connect to the database before starting the application server.
 mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
   if (err) {
