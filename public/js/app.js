c3eab077c4c184f9cb354e5686fafb24d73d7ad7
Guillaume Lambert <guillaume.p.lambert@gmail.com>
2016-08-02 17:32:59 +0200

    Updated angular app name

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-angular.module("contactsApp", ['ngRoute'])
+angular.module("pokemonManagerApp", ['ngRoute'])
     .config(function($routeProvider) {
         $routeProvider
             .when("/", {
